88f2c6cf42f69c97f856165dfde1a5a4976b135d
akshaykumar630 <akshaykumarch17@gmail.com>
2024-08-26 14:36:36 +0530

    js adding

---
+++ ext.js
@@ -0,0 +1,14 @@
+// function createItem(title, details) {
+//     const item = document.createElement('div');
+//     item.classList.add('resume-item');
+    
+//     const itemTitle = document.createElement('h3');
+//     itemTitle.textContent = title;
+//     item.appendChild(itemTitle);
+    
+//     const itemDetails = document.createElement('p');
+//     itemDetails.textContent = details;
+//     item.appendChild(itemDetails);
+
+//     return item;
+// }
